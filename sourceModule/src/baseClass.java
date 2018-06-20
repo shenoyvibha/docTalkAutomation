fd465cabb47b988730e4a821115d4b17b2083c6d
shenoyvibha <shenoy.vibha@gmail.com>
2018-06-20 12:34:21 +0530

    test cases

---
+++ sourceModule/src/baseClass.java
@@ -0,0 +1,34 @@
+import com.sun.tools.corba.se.idl.constExpr.And;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class baseClass {
+
+    public baseClass() throws MalformedURLException {
+    }
+    public static AndroidDriver getDriver() throws MalformedURLException {
+        File app = new File("/Users/vibha/apks/patient-debug.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability("deviceName", "Lol");
+        capabilities.setCapability("platformversion", "6.0.1");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "100");
+        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.1:4723/wd/hub"), capabilities);
+        return driver;
+
+    }
+    public AppiumDriver driver = getDriver();
+
+
+}
+
